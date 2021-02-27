a8829c86b51934b2c25c5be78a70bd4962a88d7d
tmiyake1824 <tsugunobu.miyake@hotmail.co.jp>
2021-02-27 23:29:23 +0900

    edit setting

--- StudyHall/settings.py
+++ StudyHall/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j@(*e%8lp1seh)_jfhmb0+#1w&eddf3ejbw)t)d@!ncl2fy&#3'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
